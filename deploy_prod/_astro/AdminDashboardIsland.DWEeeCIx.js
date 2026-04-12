b7b14758d6a18194b74d8e0f05cc2dea604ffa83
Filip <webiskopl@gmail.com>
2026-04-12 17:14:17 +0200

    Sync committed deploy artifacts with production build

--- deploy_prod/_astro/AdminDashboardIsland.DWEeeCIx.js
+++
@@ -1,1 +0,0 @@
-import"./BlobArrowIcon.CnxqTVfw.js";import{e as m}from"./AdminDashboardIsland.Db7eeYdf.js";import"./AdminPanelFrame.mIMQoKRX.js";export{m as default};
---
+++ deploy_prod/_astro/AdminDashboardIsland.DtvLoDUG.js
@@ -0,0 +1,1 @@
+import"./BlobArrowIcon.CnxqTVfw.js";import{e as m}from"./AdminDashboardIsland.Bd6U9-rI.js";import"./AdminPanelFrame.TbnSsEMX.js";export{m as default};
